4c077ddc718389f1a8fcc2a35e6b5c558eee08cd
Ryan <teelrc@gmail.com>
2017-12-17 16:28:02 -0800

    Added scripts for Stockings

---
+++ Xmas 2017/Assets/Stocking.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stocking : MonoBehaviour {
+
+	public enum StockingColors { Red,Orange,Yellow,Green,Blue,Purple};
+
+    public StockingColors strockingColor = StockingColors.Red;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
