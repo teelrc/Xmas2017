c11b78e682cd5e4e335610d688708811082582cc
Adrienne Grieco <adrienne.grieco@gmail.com>
2017-12-17 16:20:52 -0800

    adding foundation for stocking game

---
+++ Xmas 2017/Assets/DragAndDrop.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private bool _isSelected = false;
+    private float _selectX, _selectY;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        _isSelected = true;
+        var v3 = Input.mousePosition;
+        v3.z = 0.0f;
+        v3 = Camera.main.ScreenToWorldPoint(v3);
+        _selectX = v3.x - transform.position.x;
+        _selectY = v3.y - transform.position.y;
+    }
+
+    void OnMouseDrag()
+    {
+        if (_isSelected)
+        {
+            var v3 = Input.mousePosition;
+            v3.z = 0.0f;
+            v3 = Camera.main.ScreenToWorldPoint(v3);
+            transform.position = new Vector3(v3.x - _selectX, v3.y - _selectY, transform.position.z);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        _isSelected = false;
+
+        Collider2D myCollider = GetComponent<Collider2D>();
+        Collider2D[] snapColliders = new Collider2D[4];
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        int count = myCollider.OverlapCollider(contactFilter, snapColliders);
+
+        if (count > 0)
+        {
+            Vector3 destination = new Vector3(snapColliders[0].transform.position.x, snapColliders[0].transform.position.y, transform.position.z);
+            for (int i = 1; i < count; ++i)
+            {
+                Vector3 currentDest = new Vector3(snapColliders[i].transform.position.x,
+                    snapColliders[i].transform.position.y, transform.position.z);
+                if (Vector3.Distance(transform.position, destination) >
+                    Vector3.Distance(transform.position, currentDest))
+                {
+                    destination = currentDest;
+                }
+            }
+
+
+            this.transform.position = destination;
+
+        }
+    }
+}
